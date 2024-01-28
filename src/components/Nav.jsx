388d5497dea7f15667f41bcb39ad2d989ce34e71
Sunny Hundal <amandip.h@gmail.com>
2024-01-27 23:21:10 -0800

    Updated navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,67 +6,36 @@ export default function Nav() {
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
+          <div
+            className="absolute inset-y-0 left-0 flex items-center sm:hid
+den"
+          >
+            <NavLink
+              to="/"
+              className="text-white px-3 py-2 text-xl font-medium"
             >
-              <span className="sr-only">Open main menu</span>
-
-              {/* Heroicon name: menu */}
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
+              Sunny Hundal
+            </NavLink>
           </div>
 
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0 flex items-center">
-              <h1 className="text-white font-bold text-2xl">Sunny Hundal</h1>
-            </div>
-
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-
                 <NavLink
                   to="/"
-                  exact
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  aria-current="page"
+                  className="text-white px-3 py-2 text-xl font-medium hover: rounded-md bg-gray-700"
                 >
                   Home
                 </NavLink>
-
                 <NavLink
                   to="/about"
-                  exact
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  aria-current="page"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 text-xl font-medium"
                 >
                   About
                 </NavLink>
-
                 <NavLink
                   to="/contact"
-                  exact
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  aria-current="page"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 text-xl font-medium"
                 >
                   Contact
                 </NavLink>
